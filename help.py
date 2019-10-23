465c6cc85a14c76bf9273c71082611ee7c2b38c4
Zed Chance <zchance@sierracollege.edu>
2019-10-22 20:19:05 -0700

    Help message now shows on commands and categories
    generate_usage() method returns a string on how to use the command.
    I think there is something I could use to accomplish this in the
    documentation ( https://discordpy.readthedocs.io/en/latest/ext/commands/api.html#discord.ext.commands.HelpCommand.get_command_signature )
    however I can't figure out how to use it.

--- help.py
+++ help.py
@@ -4,8 +4,8 @@
 import discord
 from discord.ext import commands
 
-bot_title = 'Bot title'
-bot_description = 'Bot under development'
+bot_title = ''
+bot_description = ''
 footer = 'Use `!blue help [command]` or `!blue help [category]` for more information'
 
 class Help(commands.Cog):
@@ -20,36 +20,69 @@ class Help(commands.Cog):
     async def help_command(self, ctx, *commands : str):
         """ Shows this message """
         bot = ctx.bot
-        # destination = ctx.message.author if bot.pm_help else ctx.message.channel
         embed = discord.Embed(title=bot_title, description=bot_description)
 
         def repl(obj):
             return _mentions_transforms.get(obj.group(0), '')
+        
+        def generate_usage(command_name):
+            """ Generates a string of how to use a command """
+            temp = f'!b '
+            command = bot.get_command(command_name)
+            # Aliases
+            if len(command.aliases) == 0:
+                temp += f'{command_name}'
+            elif len(command.aliases) == 1:
+                temp += f'[{command.name}|{command.aliases[0]}]'
+            else:
+                t = '|'.join(command.aliases)
+                temp += f'[{command.name}|{t}]'
+            # Parameters
+            params = f' '
+            for param in command.clean_params:
+                print(param)
+                params += f'<{command.clean_params[param]}> '
+            temp += f'{params}'
+            return temp
 
         # Help by itself just lists our own commands.
         if len(commands) == 0:
             for cog in bot.cogs:
-                help_string = ""
+                temp = ""
                 for command in bot.get_cog(cog).get_commands():
                     if command.help is not None:
-                        help_string += f' `{command}` \0\0\0\0\0\0 {command.help}\n'
+                        temp += f' `{command}` \0\0 {command.help}\n'
                     else:
-                        help_string += f'`{command}`\n'
-                embed.add_field(name=f'**{cog}**', value=f'{help_string}', inline=True)
+                        temp += f'`{command}`\n'
+                embed.add_field(name=f'**{cog}**', value=temp, inline=True)
             embed.add_field(name= "\0", value=footer, inline=False)
-        # elif len(commands) == 1:
+        elif len(commands) == 1:
             # try to see if it is a cog name
             # name = _mention_pattern.sub(repl, commands[0])
-            # command = None
-            # if name in bot.cogs:
-                # command = bot.cogs[name]
-            # else:
-            #     command = bot.commands.get(name)
-            #     if command is None:
-            #         yield from bot.send_message(destination, bot.command_not_found.format(name))
-            #         return
+            name = commands[0]
+            command = None
+            
+            if name in bot.cogs:
+                cog = bot.get_cog(name)
+                msg = ""
+                for command in cog.get_commands():
+                    if command.help is not None:
+                        msg += f' `{command}` \0\0 {command.help}\n'
+                    else:
+                        msg += f'`{command}`\n'
+                embed.add_field(name=name, value=msg, inline=False)
+                msg = f'{cog.description}\n'
+                embed.add_field(name="\0", value=msg, inline=False)
+            else:
+                command = bot.get_command(name)
+                if command is not None:
+                    embed.add_field(name=f'**{command}**', value=f'{command.description}```{generate_usage(name)}```', inline=False)
+                else:
+                    msg = ""
+                    for com in commands:
+                        msg += f'{msg} '
+                    embed.add_field(name="Not found", value=f'Command `{msg}` not found.')
 
-            # pages = bot.formatter.format_help_for(ctx, command)
         # else:
             # name = _mention_pattern.sub(repl, commands[0])
             # command = bot.commands.get(name)
