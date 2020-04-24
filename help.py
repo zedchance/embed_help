e52d94c873f1f9441d7de64972cb46ea02b79500
Zed Chance <zchance@sierracollege.edu>
2020-04-24 12:18:51 -0700

    Add 'info' alias

--- help.py
+++ help.py
@@ -9,6 +9,7 @@ bot_title = 'Bot title'
 bot_description = ''
 bottom_info = ''
 
+
 class Help(commands.Cog):
     """ Help commands """
 
@@ -16,13 +17,14 @@ class Help(commands.Cog):
         self.bot = bot
 
     @commands.command(name='help',
-        description='Help command',
-        case_insensitive=True)
-    async def help_command(self, ctx, *commands : str):
+                      description='Help command',
+                      aliases=['info'],
+                      case_insensitive=True)
+    async def help_command(self, ctx, *commands: str):
         """ Shows this message """
         bot = ctx.bot
         embed = discord.Embed(title=bot_title, description=bot_description)
-        
+
         def generate_usage(command_name):
             """ Generates a string of how to use a command """
             temp = f'{prefix}'
@@ -71,12 +73,12 @@ class Help(commands.Cog):
                 if temp != "":
                     embed.add_field(name=f'**{cog}**', value=temp, inline=False)
             if bottom_info != "":
-                embed.add_field(name= "Info", value=bottom_info, inline=False)
+                embed.add_field(name="Info", value=bottom_info, inline=False)
         elif len(commands) == 1:
             # Try to see if it is a cog name
             name = commands[0].capitalize()
             command = None
-            
+
             if name in bot.cogs:
                 cog = bot.get_cog(name)
                 msg = generate_command_list(name)
@@ -100,9 +102,10 @@ class Help(commands.Cog):
         else:
             msg = ' '.join(commands)
             embed.add_field(name="Not found", value=f'Command/category `{msg}` not found.')
-        
+
         await ctx.send(f'{ctx.author.mention}', embed=embed)
         return
+
 
 # Cog setup
 def setup(bot):
